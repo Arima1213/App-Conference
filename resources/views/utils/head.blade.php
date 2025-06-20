07354ac4f2a785f93c0002900c7a5b0ffbe849fb
Arima1213 <arima3992@gmail.com>
2025-06-20 22:44:40 +0700

    Add conference page layout and components
    - Created index.blade.php for the conference page layout including sections for the header, slider, mission, schedule, pricing, ticket purchase, news, location, partners, and Instagram feed.
    - Added utils for header, footer, preloader, and navbar to modularize the layout.
    - Included necessary CSS links in head.blade.php for styling.
    - Implemented JavaScript includes for functionality in script.blade.php.

---
+++ resources/views/utils/head.blade.php
@@ -0,0 +1,7 @@
+<link rel="shortcut icon" href="assets/img/favicon.png">
+<!-- =================== STYLE =================== -->
+<link rel="stylesheet" type="text/css" href="assets/css/slick.min.css" />
+<link rel="stylesheet" href="assets/css/bootstrap-grid.css">
+<link href="../../use.fontawesome.com/releases/v5.10.1/css/all.css" rel="stylesheet">
+<link rel="stylesheet" href="assets/css/nice-select.css">
+<link rel="stylesheet" href="assets/css/style.css">
---
+++ resources/views/utils/navbar.blade.php
@@ -0,0 +1,45 @@
+<header class="conference-header-fixed header-fixed">
+	<a href="#" class="nav-btn">
+		<span></span>
+		<span></span>
+		<span></span>
+	</a>
+	<div class="container">
+		<div class="row conference-header-row">
+			<div class="col-sm-3 col-lg-2 col-xl-3">
+				<a href="index-2.html" class="logo"><img src="assets/img/logo-white.svg" alt="logo"></a>
+			</div>
+			<div class="col-sm-9 col-lg-8 col-xl-6">
+				<nav class="nav-menu menu">
+					<ul class="nav-list">
+						<li class="dropdown active-page">
+							<a href="#">home <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+							<ul>
+								<li><a href="index-2.html">Home Marathon</a></li>
+								<li class="active-page"><a href="conference-page.html">Home Conference</a></li>
+								<li><a href="dance-page.html">Home Dance</a></li>
+							</ul>
+						</li>
+						<li><a href="#about">about us</a></li>
+						<li><a href="#schedule">schedule</a></li>
+						<li><a href="#location">location</a></li>
+						<li><a href="#register">register</a></li>
+						<li><a href="#news">news</a></li>
+						<li class="dropdown">
+							<a href="#">pages <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+							<ul>
+								<li><a href="conference-team.html">Conference Team</a></li>
+								<li><a href="dance-team.html">Dance Teame</a></li>
+								<li><a href="blog.html">Blog</a></li>
+								<li><a href="page-error.html">Page Error 404</a></li>
+							</ul>
+						</li>
+					</ul>
+				</nav>
+			</div>
+			<div class="col-md-3 col-lg-2 col-xl-3 conference-header-btn">
+				<a href="#register" class="btn"><span>Get Tickets</span></a>
+			</div>
+		</div>
+	</div>
+</header>
