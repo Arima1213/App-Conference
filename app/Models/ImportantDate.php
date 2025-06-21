cb56b3304f6a22a793dc19c6b1d5d780da0683bf
Arima1213 <arima3992@gmail.com>
2025-06-21 15:04:03 +0700

    Define fillable attributes in ImportantDate model

--- app/Models/ImportantDate.php
+++ app/Models/ImportantDate.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class ImportantDate extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'date',
+        'description',
+    ];
 }
