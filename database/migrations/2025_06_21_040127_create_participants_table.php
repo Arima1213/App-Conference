cc3a81b7e7cfd67385d06efb36a067aca3d3da72
Arima1213 <arima3992@gmail.com>
2025-06-21 11:02:52 +0700

    Add Participant model and migration for participants table

---
+++ database/migrations/2025_06_21_040127_create_participants_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('participants', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('nik')->nullable();
+            $table->string('university')->nullable();
+            $table->string('phone');
+            $table->string('participant_code')->unique();
+            $table->foreignId('category_id')->nullable()->constrained('participant_categories');
+            $table->string('paper_title')->nullable();
+            $table->string('qrcode')->nullable(); // path ke file QR atau base64
+            $table->enum('status', ['unverified', 'verified', 'arrived'])->default('unverified');
+            $table->boolean('seminar_kit_status')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('participants');
+    }
+};
