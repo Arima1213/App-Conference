541c366bef44a7b2d56869c15e8f8b8f6e6951e1
Arima1213 <arima3992@gmail.com>
2025-06-21 15:59:56 +0700

    Refactor venue model and migration: consolidate venue handling and ensure proper naming conventions

--- app/Models/Conference.php
+++ app/Models/Conference.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Conference extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'description',
+        'schedule_id',
+        'venue_id',
+        'banner',
+        'is_active',
+    ];
+
+    public function schedule()
+    {
+        return $this->belongsTo(Schedule::class);
+    }
+
+    public function venue()
+    {
+        return $this->belongsTo(venue::class);
+    }
 }
