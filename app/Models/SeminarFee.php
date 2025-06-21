6e2c4ed500247156f60a20f2c4e961e469edffd1
Arima1213 <arima3992@gmail.com>
2025-06-21 15:12:15 +0700

    Define fillable attributes in SeminarFee model

--- app/Models/SeminarFee.php
+++ app/Models/SeminarFee.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class SeminarFee extends Model
 {
-    //
+    protected $fillable = [
+        'type',
+        'category',
+        'early_bird_price',
+        'regular_price',
+        'currency',
+    ];
 }
