60ee445ad9b6e06db40d037155d8152d6df15931
Arima1213 <arima3992@gmail.com>
2025-06-21 15:13:03 +0700

    Define fillable attributes in ParticipantCategory model

--- app/Models/ParticipantCategory.php
+++ app/Models/ParticipantCategory.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class ParticipantCategory extends Model
 {
-    //
+    protected $table = 'participant_categories';
+
+    protected $fillable = [
+        'name',
+    ];
 }
