d840eeaa63b1812c691de6b1f8a016e3a2fd4568
Arima1213 <arima3992@gmail.com>
2025-06-23 12:55:42 +0700

    feat: Reintroduce countdown timer functionality in script blade file

--- resources/views/utils/script.blade.php
+++ resources/views/utils/script.blade.php
@@ -5,3 +5,54 @@
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB4fusEY9kSwNHgtK8KOgyoTsyP5Tb2NXo"></script>
 <script src="{{ asset('assets/js/parallax.min.js') }}"></script>
 <script src="{{ asset('assets/js/scripts.js') }}"></script>
+@if ($countdownTime)
+	<script>
+		document.addEventListener("DOMContentLoaded", function() {
+			var deadline = new Date("{{ \Carbon\Carbon::parse($countdownTime)->format('Y-m-d\TH:i:s') }}");
+			initializeClock('clockdiv', deadline);
+		});
+	</script>
+@endif
+<script>
+	/*-------------------- COUNTDOWN -------------------*/
+	if ($('#clockdiv')[0]) {
+		function getTimeRemaining(endtime) {
+			var t = Date.parse(endtime) - Date.parse(new Date());
+			var seconds = Math.floor((t / 1000) % 60);
+			var minutes = Math.floor((t / 1000 / 60) % 60);
+			var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+			var days = Math.floor(t / (1000 * 60 * 60 * 24));
+			return {
+				'total': t,
+				'days': days,
+				'hours': hours,
+				'minutes': minutes,
+				'seconds': seconds
+			};
+		}
+
+		function initializeClock(id, endtime) {
+			var clock = document.getElementById(id);
+			var daysSpan = clock.querySelector('.days');
+			var hoursSpan = clock.querySelector('.hours');
+			var minutesSpan = clock.querySelector('.minutes');
+			var secondsSpan = clock.querySelector('.seconds');
+
+			function updateClock() {
+				var t = getTimeRemaining(endtime);
+
+				daysSpan.innerHTML = t.days;
+				hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+				minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+				secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+				if (t.total <= 0) {
+					clearInterval(timeinterval);
+				}
+			}
+
+			updateClock();
+			var timeinterval = setInterval(updateClock, 1000);
+		}
+	}
+</script>
