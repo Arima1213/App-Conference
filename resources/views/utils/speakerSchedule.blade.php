e6778f89ec9c05ba6037803d728248867825af5c
Arima1213 <arima3992@gmail.com>
2025-06-23 14:11:15 +0700

    feat: Refactor speaker schedule display to dynamically render schedules from conference data

--- resources/views/utils/speakerSchedule.blade.php
+++ resources/views/utils/speakerSchedule.blade.php
@@ -1,71 +1,33 @@
-<section id="schedule" class="s-speakers-schedule">
-	<div class="container">
-		<h2 class="title-conference"><span>Speaker & Schedule</span></h2>
-		<div class="speakers-timeline-cover">
-			<div class="speakers-timeline-item">
-				<div class="speakers-timeline-img">
-					<a href="conference-team.html" class="our-speaker-item">
-						<img class="rx-lazy" src="assets/img/placeholder-all.png" data-src="assets/img/speaker-1.jpg" alt="img">
-						<div class="speaker-item-info">
-							<h3 class="name">Eileen Walsh</h3>
-							<p class="prof">Business Manager</p>
+@if ($conference && $conference->schedules->count())
+	<section id="schedule" class="s-speakers-schedule">
+		<div class="container">
+			<h2 class="title-conference"><span>Speaker & Schedule</span></h2>
+			<div class="speakers-timeline-cover">
+				@foreach ($conference->schedules as $schedule)
+					@php
+						$speaker = $schedule->speaker;
+						$image = $speaker && $speaker->photo ? asset('storage/' . $speaker->photo) : asset('assets/img/placeholder-all.png');
+						$time = \Carbon\Carbon::parse($schedule->start_time)->format('H:i') . ' - ' . \Carbon\Carbon::parse($schedule->end_time)->format('H:i');
+					@endphp
+					<div class="speakers-timeline-item">
+						<div class="speakers-timeline-img">
+							<a href="#" class="our-speaker-item">
+								<img class="rx-lazy" src="{{ asset('assets/img/placeholder-all.png') }}" data-src="{{ $image }}"
+									alt="{{ $speaker->name ?? 'Speaker' }}">
+								<div class="speaker-item-info">
+									<h3 class="name">{{ $speaker->name ?? 'Unknown' }}</h3>
+									<p class="prof">{{ $speaker->position ?? '-' }}</p>
+								</div>
+							</a>
 						</div>
-					</a>
-				</div>
-				<div class="speakers-timeline-info">
-					<div class="date">8:30 AM - 9:30 AM</div>
-					<h3 class="title">Teamwork all you need <br>to know the manager</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur</p>
-				</div>
-			</div>
-			<div class="speakers-timeline-item">
-				<div class="speakers-timeline-img">
-					<a href="conference-team.html" class="our-speaker-item">
-						<img class="rx-lazy" src="assets/img/placeholder-all.png" data-src="assets/img/speaker-2.jpg" alt="img">
-						<div class="speaker-item-info">
-							<h3 class="name">David Green</h3>
-							<p class="prof">Business Manager</p>
+						<div class="speakers-timeline-info">
+							<div class="date">{{ $time }}</div>
+							<h3 class="title">{{ $schedule->title ?? 'Untitled' }}<br>{{ $schedule->subtitle ?? '' }}</h3>
+							<p>{{ $schedule->description ?? '-' }}</p>
 						</div>
-					</a>
-				</div>
-				<div class="speakers-timeline-info">
-					<div class="date">10:30 AM - 11:30 AM</div>
-					<h3 class="title">24/7 Service <br>how to make a client happy</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur</p>
-				</div>
-			</div>
-			<div class="speakers-timeline-item">
-				<div class="speakers-timeline-img">
-					<a href="conference-team.html" class="our-speaker-item">
-						<img class="rx-lazy" src="assets/img/placeholder-all.png" data-src="assets/img/speaker-3.jpg" alt="img">
-						<div class="speaker-item-info">
-							<h3 class="name">Jennifer Oliver</h3>
-							<p class="prof">Business Manager</p>
-						</div>
-					</a>
-				</div>
-				<div class="speakers-timeline-info">
-					<div class="date">12:30 AM - 13:30 AM</div>
-					<h3 class="title">Everything about <br>market analytics</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur</p>
-				</div>
-			</div>
-			<div class="speakers-timeline-item">
-				<div class="speakers-timeline-img">
-					<a href="conference-team.html" class="our-speaker-item">
-						<img class="rx-lazy" src="assets/img/placeholder-all.png" data-src="assets/img/speaker-4.jpg" alt="img">
-						<div class="speaker-item-info">
-							<h3 class="name">Paul Bates</h3>
-							<p class="prof">Business Manager</p>
-						</div>
-					</a>
-				</div>
-				<div class="speakers-timeline-info">
-					<div class="date">14:30 AM - 15:30 AM</div>
-					<h3 class="title">Career growth how to <br>become a sought-after </h3>
-					<p>Lorem ipsum dolor sit amet, consectetur</p>
-				</div>
+					</div>
+				@endforeach
 			</div>
 		</div>
-	</div>
-</section>
+	</section>
+@endif
