f488c65dbc69ace15ad852be4377c944de341104
Arima1213 <arima3992@gmail.com>
2025-06-26 15:49:17 +0700

    feat: Enhance ListEducationalInstitutions page with import and template view actions

--- app/Filament/Resources/EducationalInstitutionResource/Pages/ListEducationalInstitutions.php
+++ app/Filament/Resources/EducationalInstitutionResource/Pages/ListEducationalInstitutions.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\EducationalInstitutionResource\Pages;
 use App\Filament\Resources\EducationalInstitutionResource;
 use App\Imports\EducationalInstitutionImport;
 use Filament\Actions;
+use Filament\Actions\Action;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 use Illuminate\Support\Facades\Storage;
 use Maatwebsite\Excel\Facades\Excel;
@@ -20,24 +22,39 @@ class ListEducationalInstitutions extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
-            Actions\Action::make('Import Excel')
+            CreateAction::make(),
+            Action::make('importExcel')
+                ->label('Import from Excel')
+                ->icon('heroicon-o-arrow-up-tray')
                 ->form([
                     FileUpload::make('file')
-                        ->label('Excel File')
+                        ->label('Select Excel File')
                         ->required()
-                        ->acceptedFileTypes(['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.ms-excel']),
+                        ->acceptedFileTypes([
+                            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                            'application/vnd.ms-excel',
+                        ]),
                 ])
                 ->action(function (array $data) {
                     Excel::import(new EducationalInstitutionImport, $data['file']);
 
                     Notification::make()
-                        ->title('Import successful')
+                        ->title('Import Completed')
+                        ->body('The educational institutions have been successfully imported.')
                         ->success()
                         ->send();
                 })
                 ->modalHeading('Import Educational Institutions')
-                ->modalSubmitActionLabel('Import'),
+                ->modalSubmitActionLabel('Import')
+                ->modalCancelActionLabel('Cancel'),
+            Action::make('viewTemplate')
+                ->label('View Excel Template')
+                ->icon('heroicon-o-document-text')
+                ->modalHeading('Excel Template Format')
+                ->modalSubmitAction(false)
+                ->modalCancelActionLabel('Close')
+                ->modalWidth('lg')
+                ->modalContent(fn() => view('filament.modals.excel-template')),
         ];
     }
 }
