c64fec9904b7422bc45e8256d3a2fd32688e8f59
Arima1213 <arima3992@gmail.com>
2025-06-26 07:18:57 +0700

    feat: Add participant creation functionality with notification and redirect

--- app/Filament/Participant/Resources/ParticipantResource/Pages/CreateParticipant.php
+++ app/Filament/Participant/Resources/ParticipantResource/Pages/CreateParticipant.php
@@ -3,10 +3,32 @@
 namespace App\Filament\Participant\Resources\ParticipantResource\Pages;
 
 use App\Filament\Participant\Resources\ParticipantResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateParticipant extends CreateRecord
 {
     protected static string $resource = ParticipantResource::class;
+
+
+    protected function getRedirectUrl(): string
+    {
+        return ParticipantResource::getUrl('index');
+    }
+
+    protected function getCreatedNotificationTitle(): ?string
+    {
+        return 'Participant successfully registered!';
+    }
+
+    protected function getFormActions(): array
+    {
+        return [
+            $this->getCreateFormAction(),
+        ];
+    }
+
+    protected function hasCreateAnother(): bool
+    {
+        return false; // hilangkan tombol 'Create & create another'
+    }
 }
