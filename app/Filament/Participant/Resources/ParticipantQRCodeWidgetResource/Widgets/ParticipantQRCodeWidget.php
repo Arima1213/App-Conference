4a58f9c9b35bb91c4b3d9f4e4833eb3374c3deb5
Arima1213 <arima3992@gmail.com>
2025-06-28 18:54:05 +0700

    feat: Add ParticipantQRCodeWidget and corresponding Blade view for QR code display

---
+++ app/Filament/Participant/Resources/ParticipantQRCodeWidgetResource/Widgets/ParticipantQRCodeWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Participant\Resources\ParticipantQRCodeWidgetResource\Widgets;
+
+use Filament\Widgets\Widget;
+
+class ParticipantQRCodeWidget extends Widget
+{
+    protected static string $view = 'filament.participant.resources.participant-q-r-code-widget-resource.widgets.participant-q-r-code-widget';
+}
