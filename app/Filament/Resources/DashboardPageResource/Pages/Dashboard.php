d4495363800ae60f632f671674f4f48404057429
Arima1213 <arima3992@gmail.com>
2025-06-29 21:50:53 +0700

    chore: Add initial empty db_backup.sql and oot files

---
+++ app/Filament/Resources/DashboardPageResource/Pages/Dashboard.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Filament\Resources\DashboardPageResource\Pages;
+
+use Filament\Resources\Pages\Page;
+use App\Models\Conference;
+use Filament\Pages\Dashboard\Concerns\HasFiltersForm;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Form;
+use Filament\Widgets;
+
+class Dashboard extends Page
+{
+    protected static string $view = 'filament.resources.dashboard-page-resource.pages.dashboard';
+
+    use HasFiltersForm;
+
+    public function filtersForm(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make()
+                    ->schema([
+                        Select::make('conference_id')
+                            ->label('Conference')
+                            ->options(Conference::pluck('title', 'id'))
+                            ->searchable()
+                            ->placeholder('Select Conference'),
+                        DatePicker::make('start_date')
+                            ->label('Start Date')
+                            ->default(now()->startOfMonth()),
+                        DatePicker::make('end_date')
+                            ->label('End Date')
+                            ->default(now()->endOfMonth()),
+                    ])
+                    ->columns(3),
+            ]);
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            // Masukkan custom widget di sini jika ada
+            // Contoh:
+            // \App\Filament\Widgets\OverviewStats::class,
+        ];
+    }
+}
