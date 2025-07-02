80c73648e3efb7093ddbd9dcdac792f453292277
Arima1213 <arima3992@gmail.com>
2025-07-02 12:13:00 +0700

    feat: Refactor sponsor display logic to categorize sponsors by level

--- resources/views/utils/sponsor.blade.php
+++ resources/views/utils/sponsor.blade.php
@@ -2,19 +2,61 @@
 	<div class="container">
 		<h2 class="title-conference"><span>Our partners</span></h2>
 		<div class="clients-cover">
-			@forelse ($sponsors as $sponsor)
-				<div class="client-slide">
-					<div class="client-slide-cover">
-						<img src="{{ asset('storage/' . $sponsor->logo) }}" alt="{{ $sponsor->name }}" style="max-height: 80px;">
+
+			{{-- Gold Sponsors --}}
+			@php $goldSponsors = $sponsors->where('level', 'gold'); @endphp
+			@if ($goldSponsors->count())
+				@foreach ($goldSponsors as $sponsor)
+					<div class="client-slide">
+						<div class="client-slide-cover">
+							<img src="{{ asset('storage/' . $sponsor->logo) }}" alt="{{ $sponsor->name }}" style="max-height: 120px;">
+							<div class="sponsor-level" style="text-align:center; margin-top:8px;">
+								<span class="badge badge-gold">{{ ucfirst($sponsor->level) }}</span>
+							</div>
+						</div>
 					</div>
-				</div>
-			@empty
+				@endforeach
+			@endif
+
+			{{-- Silver Sponsors --}}
+			@php $silverSponsors = $sponsors->where('level', 'silver'); @endphp
+			@if ($silverSponsors->count())
+				@foreach ($silverSponsors as $sponsor)
+					<div class="client-slide">
+						<div class="client-slide-cover">
+							<img src="{{ asset('storage/' . $sponsor->logo) }}" alt="{{ $sponsor->name }}" style="max-height: 90px;">
+							<div class="sponsor-level" style="text-align:center; margin-top:8px;">
+								<span class="badge badge-silver">{{ ucfirst($sponsor->level) }}</span>
+							</div>
+						</div>
+					</div>
+				@endforeach
+			@endif
+
+			{{-- Bronze Sponsors --}}
+			@php $bronzeSponsors = $sponsors->where('level', 'bronze'); @endphp
+			@if ($bronzeSponsors->count())
+				@foreach ($bronzeSponsors as $sponsor)
+					<div class="client-slide">
+						<div class="client-slide-cover">
+							<img src="{{ asset('storage/' . $sponsor->logo) }}" alt="{{ $sponsor->name }}" style="max-height: 60px;">
+							<div class="sponsor-level" style="text-align:center; margin-top:8px;">
+								<span class="badge badge-bronze">{{ ucfirst($sponsor->level) }}</span>
+							</div>
+						</div>
+					</div>
+				@endforeach
+			@endif
+
+			{{-- Jika tidak ada sponsor sama sekali --}}
+			@if ($sponsors->count() == 0)
 				<div class="client-slide">
 					<div class="client-slide-cover">
 						<img src="{{ asset('assets/img/client-placeholder.svg') }}" alt="No sponsors yet">
 					</div>
 				</div>
-			@endforelse
+			@endif
+
 		</div>
 	</div>
 </section>
